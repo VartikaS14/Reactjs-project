c0924044e0d188ac53abdb8d1a8ed648d563a3ee
vartika952 <vartika952.be22@chitkara.edu.in>
2024-05-19 15:38:46 +0530

    hero section made

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero container'>
+        <div className="hero-text">
+        <h1>Design your eyeballs  will thank you for</h1>
+        <button className='btn'>Get Started Today</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        
+      
+    </div>
+  )
+}
+
+export default Hero
