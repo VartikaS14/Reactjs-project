f850a7e1e21d0fc0a891adc1bdb9aa9cbaf51365
vartika952 <vartika952.be22@chitkara.edu.in>
2024-05-20 00:11:29 +0530

    Parts  section made

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero"
 import About from "./Components/About/About";
 import Content1 from "./Components/Content1/Content1";
-
-
+import Part from "./Components/Part/Part";
+import Part1 from "./Components/Part1/Part1";
 const App =() =>
   {
     return (
@@ -13,6 +13,8 @@ const App =() =>
         <Hero/>
         <About/>
         <Content1/>
+        <Part/>
+        <Part1/>
        
 
       </div>
